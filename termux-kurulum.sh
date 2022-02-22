10b631b03e675fbef281baf8b252703d42f66a09
CANPOLAT GÖKKAYA <75932199+coderbycanpolat@users.noreply.github.com>
2022-02-23 01:24:41 +0300

    Add files via upload

---
+++ termux-kurulum.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+echo -e '\033[36;40;1m 
+
+    __ __                 __              
+   / //_/_  _________  __/ /_  ______ ___ 
+  / ,< / / / / ___/ / / / / / / / __ `__ \
+ / /| / /_/ / /  / /_/ / / /_/ / / / / / /
+/_/ |_\__,_/_/   \__,_/_/\__,_/_/ /_/ /_/ 
+
+\e[35m   İnstagram :@canpolatgkky || Telegram :@androedit
+
+
+\e[31m              ~ Coded By Canpolatgkky ~
+
+
+
+
+\e[32m[1]\e[40mTermux temel kurulum	
+	'
+
+read -p "Sayı Girin(Default=1): " islem
+if [[ $islem == 1 || $islem == 01 ]]; then
+pkg install git -y
+pkg install python python2 python3 -y
+pkg install wget -y
+pkg install curl -y
+pkg install ruby -y
+pkg install php -y
+pkg install pip pip2 -y
+pip install colorama -y
+pip install telethon -y
+pip install --upgrade pip -y
+pkg install clang -y
+pkg install vim -y
+pkg install nano -y
+apt install proot -y
+pkg install cat -y
+pkg install figlet -y
+pkg install cmatrix -y
+pkg install perl -y
+pkg install nmap -y
+pkg install openssl -y
+pkg install nodejs -y
+apt install curl -y
+apt upgrade -y
+apt install dnsutils -y
+pip install wordlist -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+apt update -y
+
+else   
+	clear
+        echo -e '\033[36;40;1m Doğru Numara Girdiğinden Emin Ol Bro....'	
+	sleep 3
+	clear 
+fi
